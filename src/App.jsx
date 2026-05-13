05a8a4332c215cf5c49fd16d0313a95c2bf4d120
Anthony Petrosino <apetrosino789@gmail.com>
2026-05-12 22:50:00 -0400

    feat: implement gha to deploy to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-600">
+    <h1 className="text-3xl font-bold text-pink-600">
       OtisPlaylists is live!
     </h1>
   )
